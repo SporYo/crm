9dae17dfb324b76bd6a0989f0d83a64c2d4f7beb
Sergey Kostin <sporeyo89@gmail.com>
2019-11-14 11:25:19 +0300

    form validation with Vuelidate

--- src/filters/date.filter.js
+++ src/filters/date.filter.js
@@ -1,18 +1,16 @@
 export default function dateFilter(value, format ='date') {
-  const options = {
-
-  }
+  const options = {}
 
   if (format.includes('date')) {
     options.day = '2-digit'
-    options.mounth = 'long'
+    options.month = 'long'
     options.year = 'numeric'
   }
 
   if (format.includes('time')) {
-    options.hour = ''
-    options.minute = ''
-    options.second = ''
+    options.hour = '2-digit'
+    options.minute = '2-digit'
+    options.second = '2-digit'
   }
 
   return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value))
