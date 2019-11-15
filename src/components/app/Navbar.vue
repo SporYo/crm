f0be7ea894a0d7f5303535db66bef169f12954f7
Sergey Kostin <sporeyo89@gmail.com>
2019-11-15 13:09:29 +0300

    created a message plugin

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -48,7 +48,7 @@ export default {
   }),
   methods: {
     logout() {
-      this.$router.push('/login?massage=logout')
+      this.$router.push('/login?message=logout')
     }
   },
   mounted() {
